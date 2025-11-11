ab3e934c33e568822e5b54680cf6902962dde191
Carter Adams <cladams7905@gmail.com>
2025-11-11 16:18:29 -0700

    fix build error with stack auth provider

--- apps/web/src/components/providers/StackAuthProvider.tsx
+++ apps/web/src/components/providers/StackAuthProvider.tsx
@@ -2,13 +2,12 @@
 
 import type { ReactNode } from "react";
 import { StackProvider } from "@stackframe/stack";
-import { getStackClientApp } from "../../server/lib/stack/client";
+import { stackClientApp } from "../../server/lib/stack/client";
 
 interface StackAuthProviderProps {
   children: ReactNode;
 }
 
 export function StackAuthProvider({ children }: StackAuthProviderProps) {
-  const stackClientApp = getStackClientApp();
   return <StackProvider app={stackClientApp}>{children}</StackProvider>;
 }
