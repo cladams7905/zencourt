9256313c83f1ba499dc53be92cef81b0c78345ce
Carter Adams <cladams7905@gmail.com>
2025-11-11 10:45:57 -0700

    add vercel.json config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   // Transpile workspace packages to include their dependencies in the bundle
-  transpilePackages: ['@zencourt/db', '@zencourt/shared'],
+  transpilePackages: ["@zencourt/db", "@zencourt/shared"],
   // Don't externalize ffmpeg/ffprobe - let webpack bundle them properly
-  serverExternalPackages: ['fluent-ffmpeg'],
+  serverExternalPackages: ["fluent-ffmpeg"],
   webpack: (config, { isServer }) => {
     if (isServer) {
       // Exclude ffmpeg/ffprobe binaries from being processed by webpack
       config.externals = config.externals || [];
       config.externals.push({
-        'fluent-ffmpeg': 'commonjs fluent-ffmpeg',
-        'ffmpeg-static': 'commonjs ffmpeg-static',
-        'ffprobe-static': 'commonjs ffprobe-static',
+        "fluent-ffmpeg": "commonjs fluent-ffmpeg",
+        "ffmpeg-static": "commonjs ffmpeg-static",
+        "ffprobe-static": "commonjs ffprobe-static"
       });
 
       // Ignore binary files in ffmpeg/ffprobe packages
@@ -21,22 +21,17 @@ const nextConfig: NextConfig = {
       config.module.rules = config.module.rules || [];
       config.module.rules.push({
         test: /node_modules\/ffmpeg-static\/.*$/,
-        type: 'asset/resource',
+        type: "asset/resource"
       });
       config.module.rules.push({
         test: /node_modules\/ffprobe-static\/.*$/,
-        type: 'asset/resource',
+        type: "asset/resource"
       });
     }
     return config;
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.public.blob.vercel-storage.com",
-        pathname: "/**"
-      },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
