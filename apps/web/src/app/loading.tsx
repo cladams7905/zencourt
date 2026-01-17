50a39892becbab30a97bd243dcd06b7ce0574afb
Carter Adams <cladams7905@gmail.com>
2026-01-17 12:50:02 -0500

    make loading spinner smaller

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -19,14 +19,14 @@ export default function Loading() {
       </div>
 
       <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-6 text-center">
-        <div className="relative flex h-32 w-32 items-center justify-center">
-          <Loader2 className="absolute h-32 w-32 animate-spin text-primary" />
+        <div className="relative flex h-20 w-20 items-center justify-center">
+          <Loader2 className="absolute h-20 w-20 animate-spin text-primary" />
           <Image
             src="/zencourt-logo.png"
             alt="Zencourt"
-            width={40}
-            height={40}
-            className="h-10 w-10 object-contain"
+            width={24}
+            height={24}
+            className="h-6 w-6 object-contain"
             priority
           />
         </div>
